6723db4b5411247cf581feeddb1089ab97857dfb
Mohamed Ayman <ma2078865@gmail.com>
2026-02-25 06:58:29 +0200

    learn how to fetch data from an API and display it in a React component using useState and useEffect hooks.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useEffect } from "react"; 
 import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Products from "./components/products/Products";
 import Cart from "./components/products/Cart";
 import Button from "./components/products/Button";
+
 
 
 
@@ -72,45 +74,74 @@ export default App; */
 
 
 
-const ShowPassword = () => {
-  const [password, setPassword] = useState('');
-  const [user, setUser] = useState('');
+// const ShowPassword = () => {
+//   const [password, setPassword] = useState('');
+//   const [user, setUser] = useState('');
   
-  const handelLogin = (e) => {
-    if(user === '' || password === ''){
-      alert('username and password are required')
-      return;
-    }
-    if(password.length < 6){
-      alert('password must be at least 6 characters')
-      return;
-    }
-    if(user.length < 3){
-      alert('username must be at least 3 characters')
-      return;
-    }
+//   const handelLogin = (e) => {
+//     if(user === '' || password === ''){
+//       alert('username and password are required')
+//       return;
+//     }
+//     if(password.length < 6){
+//       alert('password must be at least 6 characters')
+//       return;
+//     }
+//     if(user.length < 3){
+//       alert('username must be at least 3 characters')
+//       return;
+//     }
     
-    e.preventDefault();
-    console.log({user, password}); 
+//     e.preventDefault();
+//     console.log({user, password}); 
     
+//   }
+  
+//   return (
+//     <div style={{position:'absolute',top:'50%',left:'50%',transform:"translate(-50%, -50%)",textAlign:'center'}}>
+//     <form onSubmit={handelLogin}>
+//       <label htmlFor="username" style={{display:'block'}}>username</label>
+//       <input type="text" name="userName" id="username" onChange={(e)=> setUser(e.target.value)} value={user} />
+//       <label htmlFor="password">password</label>
+//       <input type="password" name="password" style={{display:'block'}} id="password" onChange={(e)=> setPassword(e.target.value)} value={password} />
+//       <button type="submit">Submit</button>
+//     </form>
+
+//     </div>
+
+
+//   )
+// }
+
+//https://jsonplaceholder.typicode.com/photos
+// learn how to fetch data from an API and display it in a React component using useState and useEffect hooks.
+
+function App (){
+const [fetchData, setFetchData] = useState([]);
+
+useEffect( () => {
+  const api = async () => {
+    try {
+      const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+      const data = await res.json()
+      setFetchData(data)
+    } catch (error) {
+      console.log(error)
+    }
   }
-  
-  return (
-    <div style={{position:'absolute',top:'50%',left:'50%',transform:"translate(-50%, -50%)",textAlign:'center'}}>
-    <form onSubmit={handelLogin}>
-      <label htmlFor="username" style={{display:'block'}}>username</label>
-      <input type="text" name="userName" id="username" onChange={(e)=> setUser(e.target.value)} value={user} />
-      <label htmlFor="password">password</label>
-      <input type="password" name="password" style={{display:'block'}} id="password" onChange={(e)=> setPassword(e.target.value)} value={password} />
-      <button type="submit">Submit</button>
-    </form>
-
-    </div>
-
-
-  )
+  api();
+}, [])
+return (
+  <div>
+    {fetchData.slice(0, 10).map((item)=>
+    <div key={item.id}>
+      <h2>{item.title}</h2>
+      <img src={item.url} alt={item.title} />
+    </div>)}
+  </div>
+)
 }
 
-export default ShowPassword;
+export default App;
 
 
